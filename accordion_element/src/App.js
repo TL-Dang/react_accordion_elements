b69d986479ffb61c365be0732b2eca62a1ac05ea
Tommy Dang <ledangt310@live.com>
2023-06-27 16:13:12 -0700

    add logic to AccordionItem for accordion animation

--- accordion_element/src/App.js
+++ accordion_element/src/App.js
@@ -1,28 +1,53 @@
+import { useState } from 'react';
 import './styles.css';
 
 const faqs = [
   {
-    title: 'Where are these chairs assembled?',
-    text: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusantium, quaerat temporibus quas dolore provident nisi ut aliquid ratione beatae sequi aspernatur veniam repellendus.',
+    title:
+      'How much wood would a woodchuck chuck if a woodchuck could chuck wood?',
+    text: 'A woodchuck would chuck as much wood as a woodchuck could chuck if a woodchuck could chuck wood.',
   },
   {
-    title: 'How long do I have to return my chair?',
-    text: 'Pariatur recusandae dignissimos fuga voluptas unde optio nesciunt commodi beatae, explicabo natus.',
+    title: 'Does Sally sell seashells by the seashore?',
+    text: 'She sells seashells by the seashore, The shells she sells are seashells, I’m sure.So if she sells seashells on the seashore, Then I’m sure she sells seashore shells.',
   },
   {
-    title: 'Do you ship to countries outside the EU?',
-    text: 'Excepturi velit laborum, perspiciatis nemo perferendis reiciendis aliquam possimus dolor sed! Dolore laborum ducimus veritatis facere molestias!',
+    title: 'Fuzzy Wuzzy Was a Bear',
+    text: 'Fuzzy Wuzzy was a bear, Fuzzy Wuzzy had no hair, Fuzzy Wuzzy wasn’t really fuzzy,Was he?',
   },
 ];
 
 export default function App() {
   return (
     <div>
-      <Accordion />
+      <Accordion data={faqs} />
     </div>
   );
 }
 
-function Accordion() {
-  return <div>TODO</div>;
+function Accordion({ data }) {
+  return (
+    <div className='accordion'>
+      {data.map((el, i) => (
+        <AccordionItem title={el.title} text={el.text} num={i} key={el.title} />
+      ))}
+    </div>
+  );
+}
+
+function AccordionItem({ num, title, text }) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  function handleToggle() {
+    setIsOpen((isOpen) => !isOpen);
+  }
+
+  return (
+    <div className={`item ${isOpen ? 'open' : ''}`} onClick={handleToggle}>
+      <p className='number'>{num < 9 ? `0${num + 1}` : num + 1}</p>
+      <p className='title'>{title}</p>
+      <p className='icon'>{isOpen ? '-' : '+'}</p>
+      {isOpen && <div className='content-box'>{text}</div>}
+    </div>
+  );
 }
